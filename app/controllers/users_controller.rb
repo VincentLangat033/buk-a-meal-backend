f72cf6cf8d49f058bc41ef591de2f5807b7898f1
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-23 21:47:42 +0300

    view existing status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,23 @@ class UsersController < ApplicationController
             render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
     end
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:name, :username, :email, :password, :password_confirmation)
+    end
 end
