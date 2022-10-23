90bdb5dd10fb32dafda2496a173becc1fdb82fd8
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-23 21:37:51 +0300

    add table fields

--- db/migrate/20221023182906_create_caterers.rb
+++ db/migrate/20221023182906_create_caterers.rb
@@ -1,7 +1,11 @@
 class CreateCaterers < ActiveRecord::Migration[7.0]
   def change
     create_table :caterers do |t|
-
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :service_name
+      t.string :password_digest
       t.timestamps
     end
   end
