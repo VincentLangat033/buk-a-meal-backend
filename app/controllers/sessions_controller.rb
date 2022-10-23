997f183990a7a29f2b31a862b510570f39f5bfa5
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-23 21:56:35 +0300

    modify sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+    def create_user
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: "email or password" }, status: :unauthorized
+        end
+    end
+    def create_caterer
+        caterer = Caterer.find_by(email: params[:email])
+        if caterer&.authenticate(params[:password])
+          session[:caterer_id] = caterer.id
+          render json: caterer, status: :created
+        else
+            render json: { error: "email or password" }, status: :unauthorized
+        end
+    end
+    
+    def destroy
+        session.delete :user_id 
+        session.delete :caterer_id
+        head :no_content
+    end
+end
