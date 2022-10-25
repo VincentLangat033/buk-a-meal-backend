43f4558688508f43ac67838d68091491713a4478
Vincent Langat <kimutailangat033@gmail.com>
2022-10-25 07:39:45 +0300

    add validation to Meal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_100549) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_034001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
