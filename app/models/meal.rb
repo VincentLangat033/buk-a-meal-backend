9f40bfd09280ee97776825aec337e27b178ee0b8
Vincent Langat <kimutailangat033@gmail.com>
2022-10-25 07:41:44 +0300

    Establish association relationships, belongs to caterer belongs to user

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,6 @@
 class Meal < ApplicationRecord
-    # belongs_to :caterer
-    # belongs_to :user
+    belongs_to :caterer
+    belongs_to :user
     validates :description,length:{ minimum:20 }
 
     
