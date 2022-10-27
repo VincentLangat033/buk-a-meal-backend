cd6664b9383f37ec3241ef1056fdc7d8d754c211
Vincent Langat <kimutailangat033@gmail.com>
2022-10-27 09:49:33 +0300

    add attributtes to meal model to be displayed

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,4 +1,4 @@
 class MealSerializer < ActiveModel::Serializer
-    attributes :id, :name, :price, :quantity, :description, :ingredients, :caterer_id
+    attributes :id, :name, :price, :quantity, :description, :ingredients, :caterer_id, :image_url, :day
 end
 
