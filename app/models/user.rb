b060ab51df9f87c3649ed667bc10d57c6e5d3de0
Vincent Langat <kimutailangat033@gmail.com>
2022-10-31 12:57:15 +0300

    validates username, uniqueness true, password length between 8 and 250

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-    # validates :username, presence: true, length: {minimum: 8}
-    validates :email, presence: true, uniqueness: true
+    # # validates :username, presence: true, length: {minimum: 8}
+    # validates :email, presence: true, uniqueness: true
+    validates :username, uniqueness: true, presence: true, length: {in: 4..25}
+    validates :email, uniqueness: true, presence: true,format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :password, length: {in: 6..250}
     has_many :orders
 end
