b9128467d3b3175d6bdb73dda7fe71a4fbee28bc
Martinpouh <martinchege2015@gmail.com>
2022-11-01 09:52:50 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :orders
-
-
  resources :meals
-
   get '/me', to: "users#show"
   post '/login', to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
