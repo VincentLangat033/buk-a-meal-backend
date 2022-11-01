477b8d1344194af0916738197eaf52ae350f7fb4
Vincent Langat <kimutailangat033@gmail.com>
2022-11-01 08:16:25 +0300

    set up categories resource route with only index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  resources :meals
+ resources :categories, only: [:index, :show]
   get '/me', to: "users#show"
   post '/login', to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
