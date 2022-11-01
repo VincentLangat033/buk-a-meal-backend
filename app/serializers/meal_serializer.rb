305ac0d4481bbbd264f067465c6ebf97656e609c
Vincent Langat <kimutailangat033@gmail.com>
2022-11-01 09:14:38 +0300

    set up belongs to relationship so as to display categories

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,4 +1,5 @@
 class MealSerializer < ActiveModel::Serializer
     attributes :id, :name, :price, :quantity, :description, :ingredients, :caterer_id, :image_url, :day
+    belongs_to :category
 end
 
