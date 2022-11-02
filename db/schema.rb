4e95a31aa291b3d9de1044dd387230a8576c8afd
Vincent Langat <kimutailangat033@gmail.com>
2022-11-02 13:12:22 +0300

    add orders rotes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_053253) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_065233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_053253) do
     t.string "day"
     t.integer "caterer_id"
     t.integer "category_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
