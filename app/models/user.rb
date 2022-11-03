c705b92cc29a86af7732088c4dfce02b8f0afdad
Vincent Langat <kimutailangat033@gmail.com>
2022-11-03 07:14:22 +0300

    check validation logic in all the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     # validates :email, presence: true, uniqueness: true
     validates :username, uniqueness: true, presence: true, length: {in: 4..25}
     validates :email, uniqueness: true, presence: true,format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
-    validates :password, length: {in: 6..250}
+    validates :password, length: {in: 4..250}
     has_many :orders
 end
