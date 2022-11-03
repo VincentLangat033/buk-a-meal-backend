71ee5e99d6565c5d64726609e134103d9eec6f04
Vincent Langat <kimutailangat033@gmail.com>
2022-11-03 06:23:07 +0300

    create seed order data

--- db/seeds.rb
+++ db/seeds.rb
@@ -715,6 +715,23 @@ Meal.create!([
 
 )
 
+Order.create!(
+  {
+    name: "Sea Prize",
+    price: "4800"
+  
+  },
+  {
+    name: "Sea Prizes",
+    price: "4800"
+  }
+
+ 
+])
+
+
+
+
 # Meal.create!(name: "Beef with pilau", price: "50", quantity: "100", description: "Beef pilau is prepared and can be served in different forms. Have a great taste of our pilau here", image_url: "https://potentash.com/wp-
 # content/uploads/2020/06/beef-pilau-e1653046248545-640x570.jpg", ingredients: "goat meat, rice and spices", caterer_id: 1)
 # Meal.create!(name: "Beef with pilau", price: "55", quantity: 80, description: "Beef pilau is prepared and can be served in different forms. Have a great taste of our pilau here", image_url: "https://potentash.com/wp-
